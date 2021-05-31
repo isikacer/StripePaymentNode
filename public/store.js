be68b393e6361f1dafdf9d8f8248c2381b0609ec
kacer <isi.kacer@gmail.com>
2021-05-31 11:30:18 +0100

    first commit

---
+++ public/store.js
@@ -0,0 +1,62 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {  
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+var stripeHandler = StripeCheckout.configure({
+    key: stripePublicKey,
+    locale: 'en',
+    token: function(token) {
+        var items = []
+        var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+        var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+        for (var i = 0; i < cartRows.length; i++) {
+            var cartRow = cartRows[i]
+            var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+            var quantity = quantityElement.value
+            var id = cartRow.dataset.itemId
+            items.push({
+                id: id,
+                quantity: quantity
+            })
+        }
+
+        fetch('/purchase', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                stripeTokenId: token.id,
+                items: items
+            })
+        }).then(function(res) {
+            return res.json()
+        }).then(function(data) {
+            alert(data.message)
+            var cartItems = document.getElementsByClassName('cart-items')[0]
+            while (cartItems.hasChildNodes()) {
+                cartItems.removeChild(cartItems.firstChild)
+            }
+            updateCartTotal()
+        }).catch(function(error) {
+            console.error(error)
+        })
+    }
+})
+
+function purchaseClicked() {
+    var priceElement = document.getElementsByClassName('cart-total-price')[0]
+    var price = parseFloat(priceElement.innerText.replace('$', '')) * 100
+    stripeHandler.open({
+        amount: price
+    })
+}
+
---
+++ server.js
@@ -0,0 +1,69 @@
+const dotenv = require('dotenv');
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+
+dotenv.config({
+    path: './.env'
+  });
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+const stripe = require('stripe')(stripeSecretKey)
+
+console.log('server started')
+
+app.set('view engine','ejs')
+app.use(express.json())
+app.use(express.static('public'))
+
+
+app.get('/', function(req, res) {
+    fs.readFile('items.json', function(error, data) {
+      if (error) {
+        res.status(500).end()
+      } else {
+        res.render('store.ejs', {
+          stripePublicKey: stripePublicKey,
+          items: JSON.parse(data)
+        })
+      }
+    })
+  })
+
+
+   app.post('/purchase', function(req, res) {
+    fs.readFile('items.json', function(error, data) {
+      if (error) {
+        res.status(500).end()
+      } else {
+        const itemsJson = JSON.parse(data)
+        const itemsArray = itemsJson.merch
+        let total = 0
+        req.body.items.forEach(function(item) {
+          const itemJson = itemsArray.find(function(i) {
+            return i.id == item.id
+          })
+          total = total + itemJson.price * item.quantity
+        })
+  
+        stripe.charges.create({
+          amount: total,
+          source: req.body.stripeTokenId,
+          currency: 'usd'
+        }).then(function() {
+          console.log('Charge Successful')
+          res.json({ message: 'Successfully purchased items' })
+        }).catch(function() {
+          console.log('Charge Fail')
+          res.status(500).end()
+        })
+      }
+    })
+  }) 
+
+
+app.listen(3000)
+
